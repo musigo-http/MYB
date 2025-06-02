64a896c416073af496bfe4da4820a3fad54abc08
mateo <mateo@kali.home>
2025-06-02 13:56:53 +0200

    ajout de la demande d'adresse lors de l'inscription

---
+++ inscription.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parrit - Inscription</title>
+</head>
+<body>
+    <p>Adresse</p>
+    <input type="text" name="" id="numeroderue" placeholder="N° et rue">
+    <input type="text" name="" id="ligneadresseaditionelle" placeholder="Ligne d'adresse additionelle"><!--complement d'adresse ex: etage2 apartement b etc etc-->
+    <input type="text" name="" id="ville" placeholder="Ville">
+    <input type="text" name="" id="pays" placeholder="Pays">
+    <input type="text" name="" id="codepostal" placeholder="Code Postal">
+</body>
+</html>
