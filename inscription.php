aca7fc69b9d92fa2bf1945ededcaebccdf5d60a2
mateo <mateo@kali.home>
2025-06-04 10:26:17 +0200

    backend inscription fini

--- inscription.php
+++ inscription.php
@@ -8,12 +8,37 @@
 <body>
     <p>Adresse</p>
     <form method="post">
+      <input type="text" name="name" id="name" placeholder="Nom, ex: Dupont" require>
+      <input type="text" name="firstname" id="firstname" placeholder="Prenom, ex: Mathieu" require>
+      <input type="text" name="email" id="email" placeholder="Email, ex: mail@exemple.com" require>
+      <input type="tel" name="numberphone" id="numberphone" placeholder="Numéro de telephone, ex: +33782421412" require>
+      <p>Date de naissance</p>
+      <input type="date" name="datedenaissance" id="datedenaissance" require>
       <input type="text" name="rue" id="rue" placeholder="Rue, ex: rue Paul Bert" require>
-      <input type="text" name="numeroderue" id="numeroderue" placeholder="numéro de domicile, ex: 2">
+      <input type="text" name="numeroderue" id="numeroderue" placeholder="numéro de domicile, ex: 2" require>
       <!--<input type="text" name="" id="ligneadresseaditionelle" placeholder="Ligne d'adresse additionelle">-->
-      <input type="text" name="ville" id="ville" placeholder="Ville, ex: Paris">
-      <input type="text" name="pays" id="pays" placeholder="Pays, ex: France">
-      <input type="text" name="codepostal" id="codepostal" placeholder="Code Postal, ex: 75003">
+
+<!--
+
+
+[Wed Jun  4 09:45:58 2025] PHP 8.2.27 Development Server (http://localhost:8000) started
+[Wed Jun  4 09:46:14 2025] [::1]:55918 Accepted
+[Wed Jun  4 09:46:25 2025] PHP Warning:  Undefined array key "rue" in /home/mateo/Desktop/parrit.ai/inscription.php on line 28
+[Wed Jun  4 09:46:25 2025] [::1]:55918 [400]: GET /inscription.php
+[Wed Jun  4 09:46:25 2025] [::1]:55918 Closing
+[Wed Jun  4 09:46:25 2025] [::1]:34794 Accepted
+[Wed Jun  4 09:46:25 2025] [::1]:34800 Accepted
+[Wed Jun  4 09:46:25 2025] [::1]:34794 [404]: GET /favicon.ico - No such file or directory
+[Wed Jun  4 09:46:25 2025] [::1]:34794 Closing
+
+
+-->
+
+
+
+      <input type="text" name="ville" id="ville" placeholder="Ville, ex: Paris" require>
+      <input type="text" name="pays" id="pays" placeholder="Pays, ex: France" require>
+      <input type="text" name="codepostal" id="codepostal" placeholder="Code Postal, ex: 75003" require>
       <button id="submit">Suivant</button>
     </form>
 </body>
@@ -49,4 +74,27 @@ echo "lng: $lng";
   echo json_encode(['error' => 'Adresse manquante']);
   exit;
 }//mettre les infos dns la db (vrai addresse lisible de tous et lat et lng une colone pour chacuns) et passer a l'etape suivante
+try{
+    $bdd = new PDO("mysql:host=localhost; unix_socket=/run/mysqld/mysqld.sock; dbname=bdd;", "root", "Mat.at89");
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $bdd->prepare("INSERT INTO utilisateurs (email, prenom, nom, adresse, numero_telephone, birthday, lat, lng)
+                           VALUES (:email, :prenom, :nom, :adresse, :numero_telephone, :birthday, :lat, :lng)");
+
+    $stmt->execute([
+      'email' => $_POST["email"],
+      'prenom' => $_POST["firstname"],
+      'nom' => $_POST["name"],
+      'adresse' => $address,
+      'numero_telephone' => $_POST["numberphone"],
+      'birthday' => $_POST["datedenaissance"],
+      'lat' => $lng,
+      'lng' => $lat
+  ]);
+}
+catch(PDOException $e){
+    echo "ERREUR : ".$e->getMessage();
+}
+
+header("Location: index.php");
+
 ?>
