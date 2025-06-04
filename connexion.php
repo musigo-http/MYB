a5952d277a1cef16696b4c9e28ff2a84aede4942
mateo <mateo@kali.home>
2025-06-04 13:03:33 +0200

    rajout de la connexion distante au serveur pour se connecter a la db on ne passe plus par localhost mais musigo.duckdns.org

--- connexion.php
+++ connexion.php
@@ -16,11 +16,11 @@
 
 <?php
 try{
-    $bdd = new PDO("mysql:host=localhost; unix_socket=/run/mysqld/mysqld.sock; dbname=bdd;", "root", "Mat.at89");
+    $bdd = new PDO("mysql:host=musigo.duckdns.org; unix_socket=/run/mysqld/mysqld.sock; dbname=bdd;", "root", "Mat.at89");
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $stmt = $bdd->prepare("INSERT INTO utilisateurs (email, prenom, nom, adresse, numero_telephone, birthday, lat, lng)
                            VALUES (:email, :prenom, :nom, :adresse, :numero_telephone, :birthday, :lat, :lng)");
-                           
+
   $_SESSION["email"]=$_POST["email"];
 }
 catch(PDOException $e){
