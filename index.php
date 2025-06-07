a42fa457682ade0d36891393d032f1b6cb9f3993
mateo <mateo@kali.home>
2025-06-07 19:24:23 +0200

    front demande profil

--- index.php
+++ index.php
@@ -1,12 +1,32 @@
 <?php
 session_start();
 
+/*
+    $bdd = new PDO("mysql:host=musigo.duckdns.org; unix_socket=/run/mysqld/mysqld.sock; dbname=bdd;", "root", "Mat.at89");
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $bdd->prepare("INSERT INTO utilisateurs (email, prenom, nom, adresse, numero_telephone, birthday, lat, lng, password)
+                           VALUES (:email, :prenom, :nom, :adresse, :numero_telephone, :birthday, :lat, :lng, :password)");
+
+    $stmt->execute([
+      'email' => $_POST["email"],
+      'prenom' => $_POST["firstname"],
+      'nom' => $_POST["name"],
+      'adresse' => $address,
+      'numero_telephone' => $_POST["numberphone"],
+      'birthday' => $_POST["datedenaissance"],
+      'lat' => $lng,
+      'lng' => $lat,
+      'password' => password_hash($_POST["password"], PASSWORD_DEFAULT)
+  ]);
+*/
+
 if(!$_SESSION["email"]){
     header("Location: /connexion.php");
     exit;
-}elseif($_SESSION["plan"] == "None"){
-    header("Location: /plan.php");
-}
+}//elseif(db plan = None){
+//    header("Location: /plan.php");
+//    exit;
+//}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
--- plan.php
+++ plan.php
@@ -7,9 +7,9 @@
 </head>
 <body>
     <h1>choose your parrit plan here:</h1>
-    <div class="plan1">Free</div><!--les avanteages et inconveniant a avoir un compte free avec des emoji croix rouge et validé vert pour dire se qu'il a et n'a pas comme avantage comme sur le screen d'hostinger-->
-    <div class="plan2"></div>
-    <div class="plan3"></div>
-    <div class="plan4"></div>
+    <div class="plan1">Free 0€</div><!--les avanteages et inconveniant a avoir un compte free avec des emoji croix rouge et validé vert pour dire se qu'il a et n'a pas comme avantage comme sur le screen d'hostinger-->
+    <div class="plan2">Basic 10€</div><!--des qu'il clique sur le plan qu'il veux sa me redirige vers la page upgrade plan avec le plan souhaité pour passer au payment-->
+    <div class="plan3">Advanced 50€</div>
+    <div class="plan4">Premium 100€</div>
 </body>
 </html>
