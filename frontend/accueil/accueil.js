7fcfd7c6a12f8b5fc677bc491fb026ea4109ee70
mateo <mateo@kali.home>
2025-07-24 15:09:41 +0200

    add redirection for link all pages

---
+++ frontend/accueil/accueil.js
@@ -0,0 +1,6 @@
+document.getElementById("inscriptionbtn").addEventListener("click", function(){
+    window.location.href = "../inscription/inscription.html";
+});
+document.getElementById("connexionbtn").addEventListener("click", function(){
+    window.location.href = "../connexion/connexion.html";
+});
---
+++ frontend/inscription/main.js
@@ -0,0 +1,6 @@
+document.getElementById("green1").addEventListener("click", function(){
+    window.location.href = "acheteur/acheteur.html";
+});
+document.getElementById("green2").addEventListener("click", function(){
+    window.location.href = "offreur/offreur.html";
+});
